5481f2d1bff69c895a4f764c8a09f75d306705ac
Pat Kelly <patrick.kelly.1088@gmail.com>
2022-03-17 23:12:50 -0400

    slideshow for home page

---
+++ scripts/slideShow.js
@@ -0,0 +1,17 @@
+'use strict';
+let slideIndex = 0;
+
+const showSlides = () => {
+    let slides = document.querySelectorAll('.mySlides');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display="block";
+    slideIndex++;
+    if (slideIndex === slides.length) {
+        slideIndex = 0;
+    }
+    setTimeout(showSlides, 10000);
+}
+
+window.onload = showSlides();
