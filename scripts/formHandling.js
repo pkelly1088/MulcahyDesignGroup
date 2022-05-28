339288b651cf17cd5df8ecd98cdf1a7705525c6c
Pat Kelly <patrick.kelly.1088@gmail.com>
2022-05-27 22:28:44 -0400

    updated featured projects pages and added new images, and revised eexisting projects pages images gallery

--- scripts/formHandling.js
+++ scripts/formHandling.js
@@ -1,26 +1,20 @@
 'use strict';
 
-const submitBtn = document.querySelector('.submitBtn');
+const form = document.getElementById("contact-form"); 
 
 const sendMail = (mail) => {
-    console.log(mail);
-    // fetch("/send", {
-    //     method: 'POST',
-    //     body: mail,
-    // }).then(response => {
-    //     return response.json();
-    // });
-};
+    fetch("/send", {
+      method: "post",
+      body: mail,
+    }).then((response) => {
+      return response.json();
+    });
+  };
 
 const formEvent = submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
-    let mail = {
-        name: document.querySelector('#name').value,
-        email: document.querySelector('#email').value,
-        phone: document.querySelector('#phone').value,
-        message: document.querySelector('#message').value,
-    };
+    let mail = new FormData(form);
 
     sendMail(mail);
 })
