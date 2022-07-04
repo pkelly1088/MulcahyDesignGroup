1a8892ae95c2f0a1182e5e22c29d254ddff539ea
Pat Kelly <patrick.kelly.1088@gmail.com>
2022-07-03 22:17:31 -0400

    revise projects and add contact info

--- scripts/formHandling.js
+++ scripts/formHandling.js
@@ -1,20 +1,31 @@
 'use strict';
 
 const form = document.getElementById("contact-form"); 
+const userName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submitBtn");
 
 const sendMail = (mail) => {
-    fetch("/send", {
-      method: "post",
-      body: mail,
-    }).then((response) => {
-      return response.json();
-    });
-  };
+  fetch("/send", {
+    method: "post",
+    body: mail,
+
+  }).then((response) => {
+    return response.json();
+  });
+};
 
 const formEvent = submitBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let mail = new FormData(form);
+  let mail = {
+    name: userName,
+    email: email,
+    phone: phone,
+    message: message
+  }
 
-    sendMail(mail);
+  sendMail(mail);
 })
